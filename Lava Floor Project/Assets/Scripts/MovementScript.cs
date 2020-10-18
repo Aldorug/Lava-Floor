88fe276d0c5a115cda7add07355b98d7968a167f
Carlos <carlos4torres_12@hotmail.com>
2020-10-18 16:34:46 -0400

    Movement Update and Buttons Working

--- Lava Floor Project/Assets/Scripts/MovementScript.cs
+++ Lava Floor Project/Assets/Scripts/MovementScript.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class MovementScript : MonoBehaviour
 {
+    [Header("Movement")]
     public float jumpHeight;
-    public float movementSpeed;
+    public float acceleration;
+    public float deceleration;
+    public float maxSpeed;
 
     private Rigidbody2D playerBody;
+    private float curHorSpeed;
+    private float horzDir;
 
     private void Start()
     {
@@ -16,24 +21,17 @@ public class MovementScript : MonoBehaviour
     }
 
     // If we were to make the movement for pc along with changing private move and jump to public. This function is for testing movement/jumping and can be deleted after polishing.
-    private void Update()
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //        Jump();
+
+    //    horzDir = Input.GetAxisRaw("Horizontal");
+    //}
+
+    public void Move(int xDirection)
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-            Jump();
-
-        Move(0);
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-            Move(-1);
-
-        if (Input.GetKey(KeyCode.RightArrow))
-            Move(1);
-    }
-
-    public void Move(int aDirection)
-    {
-        Vector2 direction = new Vector2(aDirection, 0) * movementSpeed * Time.deltaTime;
-        playerBody.velocity += direction;
+        horzDir = xDirection;
     }
 
     public void Jump()
@@ -42,5 +40,32 @@ public class MovementScript : MonoBehaviour
         // playerBody.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
         playerBody.velocity = new Vector2(playerBody.velocity.x, jumpHeight); // Forcemode.impulse applies the changes from multiplying jump height on the player object instantly, as it is usually applied gradually over time
         // Will add check requirement for if the player is not in the air before letting you jump
+    }
+
+    private void FixedUpdate()
+    {
+        if (horzDir != 0)
+        {
+            curHorSpeed += acceleration * horzDir;
+
+            //Mathf.Clamp is shorthand for this:
+            //if (curHorSpeed < -maxSpeed)
+            //    curHorSpeed = -maxSpeed;
+            //else if (curHorSpeed > maxSpeed)
+            //    curHorSpeed = maxSpeed;
+
+            curHorSpeed = Mathf.Clamp(curHorSpeed, -maxSpeed, maxSpeed);
+        }
+        else
+        {
+            //Mathf.Max is shorthand for this:
+            //curHorSpeed -= deceleration;
+            //if (curHorSpeed < 0)
+            //    curHorSpeed = 0;
+
+            curHorSpeed = Mathf.Max(0, curHorSpeed - deceleration);
+        }
+
+        playerBody.velocity = new Vector2(curHorSpeed, playerBody.velocity.y);
     }
 }
