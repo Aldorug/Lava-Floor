6b7f5adc60804df7d579b772822580817f5e442a
Aldorug <54644502+Aldorug@users.noreply.github.com>
2020-10-18 21:55:29 -0400

    Falling Platform

---
+++ Lava Floor Project/Assets/Scripts/TempPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempPlatform : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float standingTime;
+    public float destroyTime;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Invoke("DropPlatform", 3f);
+            Destroy(gameObject, 5f);
+        }
+    }
+
+    void DropPlatform()
+    {
+        rb.isKinematic = false;
+    }
+}
