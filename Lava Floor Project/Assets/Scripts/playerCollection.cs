370e8ef9b6d681c2895b9d4e03fa867dcb4fb8a4
Carlos <carlos4torres_12@hotmail.com>
2020-10-23 19:51:13 -0400

    Minor Additions

---
+++ Lava Floor Project/Assets/Scripts/playerCollection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollection : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+   
+   //sets player gravity on trigger overlap 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Gravity"))
+        {
+            other.gameObject.SetActive(false);
+            rb.gravityScale *= -1;
+        }
+    }
+
+    
+
+}
