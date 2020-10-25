fd4e24db19e24c44628e7ae2b8fb52fc659b02e2
Aldorug <54644502+Aldorug@users.noreply.github.com>
2020-10-25 17:28:12 -0400

    PauseMenu

---
+++ Lava Floor Project/Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    public GameObject menuPause;
+    void Start()
+    {
+        menuPause.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void PauseGame()
+    {
+        menuPause.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        menuPause.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
