f5e5915a096ead5ade3827e66c3c9e42a3cf47dc
Aldorug <54644502+Aldorug@users.noreply.github.com>
2020-10-30 16:07:22 -0400

    Death mechanic and menu, score UI, pickups to give score

---
+++ Lava Floor Project/Assets/Scripts/deathMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class deathMenu : MonoBehaviour
+{
+    public GameObject menuDeath;
+    void Start()
+    {
+        menuDeath.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void goToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+
+    }
+}
--- Lava Floor Project/Assets/Scripts/playerCollection.cs
+++ Lava Floor Project/Assets/Scripts/playerCollection.cs
@@ -1,25 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerCollection : MonoBehaviour
 {
     private Rigidbody2D rb;
+
+    public GameObject playerDeath;
+    public GameObject deathMenu;
+
+    public float scoreValue;
+    public Text scoreText;
     
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        scoreText.text = "Score: " + scoreValue.ToString();
     }
 
    
-   //sets player gravity on trigger overlap 
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
+        //sets player gravity on trigger overlap
         if (other.gameObject.CompareTag("Gravity"))
         {
             other.gameObject.SetActive(false);
             rb.gravityScale *= -1;
+        }
+
+        //sets scoreValue for UI
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+            scoreValue += 1;
+            scoreText.text = "Score: " + scoreValue.ToString();
+
+        }
+
+        //Kills player and brings up death menu
+        if (other.gameObject.CompareTag("Death"))
+        {
+            playerDeath.SetActive(false);
+            deathMenu.SetActive(true);
+            Time.timeScale = 0f;
         }
     }
 
