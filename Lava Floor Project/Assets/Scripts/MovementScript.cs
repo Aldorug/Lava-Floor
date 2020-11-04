6c502e5b0ea06fee620b36212376550568a10ec8
Carlos <carlos4torres_12@hotmail.com>
2020-11-04 12:21:54 -0500

    Alpha Path Update

--- Lava Floor Project/Assets/Scripts/MovementScript.cs
+++ Lava Floor Project/Assets/Scripts/MovementScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class MovementScript : MonoBehaviour
