323b1f77361cd3d2824b080182774a70ea1ffea5
Aldorug <54644502+Aldorug@users.noreply.github.com>
2020-11-16 11:54:14 -0500

    Game timer and win screen

--- Lava Floor Project/Assets/Scripts/TimerController.cs
+++ Lava Floor Project/Assets/Scripts/TimerController.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TimerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float countdownTime;
+    public Text countdownText;
+    public GameObject winScreen;
+
+    private void Start()
     {
-        
+        winScreen.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        countdownTime -= 1 * Time.deltaTime;
+        countdownText.text = "Time left: " + countdownTime.ToString("F0");
+
+        if(countdownTime <= 0)
+        {
+            Time.timeScale = 0f;
+            winScreen.SetActive(true);
+
+        }
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
